2f87aaec5fa3894c7368ad524d43ebebea6e527e
atimchik <indatim@gmail.com>
2023-03-08 18:52:38 +0200

    goit-js-hw-08
    Задачі 1,2,3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,70 +1,153 @@
-//// ⚡ =========== Робочій варіант ('msg' & 'email') ========
+//// ⚡ =========== Робочій варіант ('msg' & 'email') => JSON ========
 
 import throttle from 'lodash.throttle';
 
-const STORAGE_KEY_MSG = 'fdbk-msg';
-const STORAGE_KEY_EMAIL = 'fdbk-email';
+const STORAGE_KEY = 'feedback-form-state';
+const email = document.querySelector('input[name="email"]');
+const message = document.querySelector('textarea[name="message"]');
 
-const formData = {};
+form = document.querySelector('.feedback-form');
 
-const refs = {
-    form: document.querySelector('.feedback-form'),
-    email: document.querySelector('input[name="email"]'),
-    textarea: document.querySelector('textarea[name="message"]'),
-};
-
-refs.form.addEventListener('submit', onFormSubmit);
-refs.email.addEventListener('input', onEmailAreaInput);
-refs.textarea.addEventListener('input', throttle(onTextAreaInput, 500));
-filledTextarea();
-filledEmailarea();
+let formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
 savedData();
 
-refs.form.addEventListener('input', throttle(e => {
+form.addEventListener('input', throttle(e => {
     formData[e.target.name] = e.target.value;
-}, 500)
-)
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}, 500));
 
-function onEmailAreaInput(e) {
-    const email = e.target.value;
-    localStorage.setItem(STORAGE_KEY_EMAIL, email);
-}
-
-function onTextAreaInput(e) {
-    const message = e.target.value;
-    localStorage.setItem(STORAGE_KEY_MSG, message);
-}
-
-function onFormSubmit(e) {
-    console.log(formData);
+form.addEventListener('submit', e => {
     e.preventDefault();
+
+    if (email.value === '' || message.value === '') {
+    return alert('Заповніть, будь-ласка, всі поля!');
+  } else console.log({ email: email.value, message: message.value });
+
     e.currentTarget.reset();
-    localStorage.removeItem(STORAGE_KEY_EMAIL);
-    localStorage.removeItem(STORAGE_KEY_MSG);
-}
+    localStorage.removeItem(STORAGE_KEY);
+});
 
 function savedData() {
-    const storageData = localStorage.getItem(formData);
-    if (storageData) {[e.target.name] = e.target.value}
+    let storageData = localStorage.getItem(STORAGE_KEY);
+    if (storageData) {
+        storageData = JSON.parse(storageData);
+        Object.entries(storageData).forEach(([name, value]) => {
+            form.elements[name].value = value;
+        });
+}
 }
 
-function filledTextarea() {
-    const savedMessage = localStorage.getItem(STORAGE_KEY_MSG);
+//// 🤖 =========== Робочій варіант ('msg' & 'email') JSON ========
+
+// import throttle from 'lodash.throttle';
+
+// const STORAGE_KEY = 'feedback-form-state';
+
+// form = document.querySelector('.feedback-form');
+
+// form.addEventListener('input', throttle(onTextAreaInput, 500));
+// form.addEventListener('submit', onFormSubmit);
+
+// let formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+// const { email, message } = form.elements;
+// savedData();
+
+// function onTextAreaInput() {
+//   formData = { email: email.value, message: message.value };
+//   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+// }
+
+// function onFormSubmit(e) {
+//     e.preventDefault();
+//     console.log({ email: email.value, message: message.value });
+
+//     if (email.value === '' || message.value === '') {
+//     return alert('Заповніть, будь-ласка, всі поля!');
+//   }
+
+//     e.currentTarget.reset();
+//     localStorage.removeItem(STORAGE_KEY);
+// }
+
+// function savedData() {
+//     let storageData = localStorage.getItem(STORAGE_KEY);
+//     if (storageData) {
+//         storageData = JSON.parse(storageData);
+//         Object.entries(storageData).forEach(([name, value]) => {
+//             form.elements[name].value = value;
+//         });
+// }
+// }
+
+
+
+// 🤖 =========== Робочій варіант ('msg' & 'email') (Репета YouTube) ========
+
+// const STORAGE_KEY_MSG = 'fdbk-msg';
+// const STORAGE_KEY_EMAIL = 'fdbk-email';
+
+// const formData = {};
+
+// const refs = {
+//     form: document.querySelector('.feedback-form'),
+//     email: document.querySelector('input[name="email"]'),
+//     textarea: document.querySelector('textarea[name="message"]'),
+// };
+
+// refs.form.addEventListener('submit', onFormSubmit);
+// refs.email.addEventListener('input', onEmailAreaInput);
+// refs.textarea.addEventListener('input', throttle(onTextAreaInput, 500));
+// filledTextarea();
+// filledEmailarea();
+// savedData();
+
+// refs.form.addEventListener('input', throttle(e => {
+//     formData[e.target.name] = e.target.value;
+// }, 500)
+// )
+
+// function onEmailAreaInput(e) {
+//     const email = e.target.value;
+//     localStorage.setItem(STORAGE_KEY_EMAIL, email);
+// }
+
+// function onTextAreaInput(e) {
+//     const message = e.target.value;
+//     localStorage.setItem(STORAGE_KEY_MSG, message);
+// }
+
+// function onFormSubmit(e) {
+//     console.log(formData);
+//     e.preventDefault();
+//     e.currentTarget.reset();
+//     localStorage.removeItem(STORAGE_KEY_EMAIL);
+//     localStorage.removeItem(STORAGE_KEY_MSG);
+// }
+
+// function savedData() {
+//     const storageData = localStorage.getItem(formData);
+//     if (storageData) {[e.target.name] = e.target.value}
+// }
+
+// function filledTextarea() {
+//     const savedMessage = localStorage.getItem(STORAGE_KEY_MSG);
  
-    if (savedMessage) {
-        refs.textarea.value = savedMessage;
-    }
-}
+//     if (savedMessage) {
+//         refs.textarea.value = savedMessage;
+//     }
+// }
 
-function filledEmailarea() {
-    const savedMail = localStorage.getItem(STORAGE_KEY_EMAIL);
+// function filledEmailarea() {
+//     const savedMail = localStorage.getItem(STORAGE_KEY_EMAIL);
  
-    if (savedMail) {
-        refs.email.value = savedMail;
-    }
-}
+//     if (savedMail) {
+//         refs.email.value = savedMail;
+//     }
+// }
 
-//// 🤖 =========== Робочій варіант з 'msg' & 'email' ===============
+
+
+//// 🤖 =========== Робочій варіант з 'msg' & 'email' (Репета YouTube) ===============
 
 // import throttle from 'lodash.throttle';
 
@@ -116,7 +199,9 @@ function filledEmailarea() {
 //     }
 // }
 
-//// 🤖 =========== Робочій варіант без 'email ===================
+
+
+//// 🤖 =========== Робочій варіант без 'email (Репета YouTube) ===================
  
 // import throttle from 'lodash.throttle';
 
